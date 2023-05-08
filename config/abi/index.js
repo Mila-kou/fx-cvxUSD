f9acd64ab88b9eb9b2976241401aa1dfc9012aab
fefe <chomkw@gmail.com>
2023-05-08 22:09:08 +0800

    fix

--- config/abi/index.js
+++ config/abi/index.js
@@ -2,7 +2,7 @@ import erc20ABI from './ERC20.json'
 import multiCallABI from './MultiCall.json'
 
 //IDO
-import IdoSale from './ido/idoSale.json'
+import IdoSale from './ido/IdoSale.json'
 import IdoGovernanceVesting from './ido/IdoGovernanceVesting.json'
 
 export default {
