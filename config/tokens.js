7e8c8484e1549da01ce5969d69f9670db183f9cb
Andrew <Andrew@aladdin>
2023-05-11 11:34:01 +0800

    Update fundsRaised config

--- config/tokens.js
+++ config/tokens.js
@@ -2,7 +2,7 @@ const contracts = {
   eth: '0x0000000000000000000000000000000000000000',
   multiCall: '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
   idoSale: '0x6AbcC0404301b947a1bC5CB562B6E12c67d1c326',
-  idoRewardToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // cvx
+  fundsRaisedToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // cvx
 }
 
 const tokens = {
