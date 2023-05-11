999e3e2427c1a60573e08e53fdc8a0eb7ad6893a
fefe <chomkw@gmail.com>
2023-05-11 22:01:18 +0800

    update

--- modules/home/components/Redeem/index.jsx
+++ modules/home/components/Redeem/index.jsx
@@ -8,7 +8,6 @@ import { cBN, fb4 } from '@/utils/index'
 import { useToken } from '@/hooks/useTokenInfo'
 import NoPayableAction, { noPayableErrorAction } from '@/utils/noPayableAction'
 import { getGas } from '@/utils/gas'
-import Tabs from '../Tabs'
 import DetailCollapse from '../DetailCollapse'
 import styles from './styles.module.scss'
 
