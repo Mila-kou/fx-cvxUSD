039eaf8f932a489ff6f3bb16ae8d31bc0867d598
Andrew <Andrew@aladdin>
2023-05-12 12:12:46 +0800

    Update fx contract

--- config/abi/index.js
+++ config/abi/index.js
@@ -5,9 +5,20 @@ import multiCallABI from './MultiCall.json'
 import IdoSale from './ido/IdoSale.json'
 import IdoGovernanceVesting from './ido/IdoGovernanceVesting.json'
 
+//fxETH
+import FX_Market from './fx/Market.json'
+import FX_FractionalToken from './fx/FractionalToken.json'
+import FX_LeveragedToken from './fx/LeveragedToken.json'
+import FX_Treasury from './fx/Treasury.json'
+
 export default {
   erc20ABI,
   multiCallABI,
   IdoSale,
   IdoGovernanceVesting,
+
+  FX_Market,
+  FX_FractionalToken,
+  FX_LeveragedToken,
+  FX_Treasury
 }
