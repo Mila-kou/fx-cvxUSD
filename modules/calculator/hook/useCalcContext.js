395a46494621e37b78f7e9075e6c3daddb96bd52
fefe <chomkw@gmail.com>
2023-07-02 11:07:12 +0800

    update

--- modules/calculator/hook/useCalcContext.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, createContext, useContext, useMemo } from 'react'
-
-export const Context = createContext(null)
-
-const useCalcContext = () => useContext(Context)
-
-export default useCalcContext
