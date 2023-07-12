8d96691a1e5daec3838b33359506879804f81aa3
Andrew <Andrew@aladdin>
2023-07-12 11:47:08 +0800

    Update config

--- config/network/mainnet-fork.js
+++ config/network/mainnet-fork.js
@@ -18,26 +18,26 @@ const explorerUri = 'https://www.etherscan.io'
 //   rpcUrl: 'https://apitest.aladdin.club/rpc',
 // }
 const chainInfo = {
-  id: '0x2707',
+  id: '0x1',
   token: 'ETH',
   label: 'Mainnet Fork',
-  rpcUrl: 'https://rpc.phalcon.xyz/rpc_942dd1172f3349ce8ff1d1a6bbffaac2',
+  rpcUrl: 'https://rpc.phalcon.xyz/rpc_f459d40bea3447cebc00767d6257248f',
 }
 
 const contracts = {
   ...tokensInfo.contracts,
 
-  ChainlinkTwapOracleV3: '0xf651a9E5eF8a4E35133e91a851838638853814ae',
-  fETH: '0x056cdD95DdF742DB771F5A1F76aF6C39987Cc9E3',
-  xETH: '0x7f12E5c14AcED183FD6b9ECf82E5F481F284dB41',
-  fx_Market: '0xaE942FBB54cB3C0a2B9F65090bE6dfc487CeC84A',
-  fx_stETHTreasury: '0xa8F4242793b65fF4E939C486b88E35D1a216B50a',
-  fx_stETHGateway: '0x43697A3a9003F42A7518c1D12b9051972B259a8d',
+  ChainlinkTwapOracleV3: '0x1109E1f9e6D4A15EabAdb22dE11B3077DD2be793',
+  fETH: '0xd6e6561Ed9D4a041a2e0c771A06aCCA82765635C',
+  xETH: '0x1CCdE657685926e342D12dF5F33d555985FBd0C3',
+  fx_Market: '0x3CB1ee412b295aC5B45952658fD70c2DeEc3B59e',
+  fx_stETHTreasury: '0x79e3868f36E2F38d2D4C71190D259b614d1d5Fe2',
+  fx_stETHGateway: '0x58A76E9C37B89718fE05446C19850249B9564C5B',
 
-  fx_StabilityPool: '0x57c9f5a2fC8D4fEd08e563Efc817Af89C4dE9dAf',
+  fx_StabilityPool: '0x7d42f87D19D872B5d343aaC09D8436ab276F9d38',
 
-  LiquidatorWithBonusToken: '0x0FD4653B77565C4e3a0580F87B521403762f638E',
-  wstETHWrapper: '0x72d62875684d7A6b64410E49A92fC43de903C06a'
+  LiquidatorWithBonusToken: '0x2394b0433Bd5769Dc16FdBaf5d24f862fc855fFc',
+  wstETHWrapper: '0x0338B5CCEe1B9297d242A2f0836379470D2e857C'
 
   // fx_Treasury: '0x908f0F329d4cab59Ae962b9ba119d7524fDF002B',
   // fx_ETHGateway: '0x38E0C09F0827326d1Cd603Da150346b2597b2792',
@@ -46,8 +46,8 @@ const contracts = {
 
 const tokens = {
   ...tokensInfo.tokens,
-  fETH: '0x056cdD95DdF742DB771F5A1F76aF6C39987Cc9E3',
-  xETH: '0x7f12E5c14AcED183FD6b9ECf82E5F481F284dB41',
+  fETH: '0xd6e6561Ed9D4a041a2e0c771A06aCCA82765635C',
+  xETH: '0x1CCdE657685926e342D12dF5F33d555985FBd0C3',
 }
 
 const TOKENS_INFO = {
