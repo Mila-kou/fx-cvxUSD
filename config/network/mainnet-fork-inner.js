44bdfaa42458423fb13137c4906b93f6cac756eb
fefe <chomkw@gmail.com>
2023-07-16 00:05:01 +0800

    fix

--- config/network/mainnet-fork-inner.js
+++ config/network/mainnet-fork-inner.js
@@ -21,23 +21,23 @@ const chainInfo = {
   id: '0x1',
   token: 'ETH',
   label: 'Phalcon-Inner',
-  rpcUrl: 'https://rpc.phalcon.xyz/rpc_f459d40bea3447cebc00767d6257248f',
+  rpcUrl: 'https://rpc.phalcon.xyz/rpc_999ebf7d68bb4a2e9c81c59c3e597280',
 }
 
 const contracts = {
   ...tokensInfo.contracts,
 
-  ChainlinkTwapOracleV3: '0xdf0Ed4f20E2e01CF8c97a94F52061EF2a50b5Abd',
-  fETH: '0x6b8dBaB9605FD4a1007e6F64176DB64482B8dB47',
-  xETH: '0x828849cD2f08B3c1ACaD6587B3adb470D870DE16',
-  fx_Market: '0xb33fDeB819eF5E00cd47B8939b43f41851fd6C1B',
-  fx_stETHTreasury: '0x41f959F53257965Dcc6b22C8326b0EBf213D7F09',
-  fx_stETHGateway: '0x5B90385dad18c2f19EEb67345713afE93eaDe748',
+  ChainlinkTwapOracleV3: '0x1109E1f9e6D4A15EabAdb22dE11B3077DD2be793',
+  fETH: '0xd6e6561Ed9D4a041a2e0c771A06aCCA82765635C',
+  xETH: '0x1CCdE657685926e342D12dF5F33d555985FBd0C3',
+  fx_Market: '0x3CB1ee412b295aC5B45952658fD70c2DeEc3B59e',
+  fx_stETHTreasury: '0x79e3868f36E2F38d2D4C71190D259b614d1d5Fe2',
+  fx_stETHGateway: '0x58A76E9C37B89718fE05446C19850249B9564C5B',
 
-  fx_StabilityPool: '0x79ec7351773228d79FA585d770588c0D6A1Ad60C',
+  fx_StabilityPool: '0x7d42f87D19D872B5d343aaC09D8436ab276F9d38',
 
-  LiquidatorWithBonusToken: '0xb890e6610369FCa71A2d00dB7DBB55f6ea05e1fb',
-  wstETHWrapper: '0x5979651d542C058A2917be11804D05a652197C80'
+  LiquidatorWithBonusToken: '0x2394b0433Bd5769Dc16FdBaf5d24f862fc855fFc',
+  wstETHWrapper: '0x0338B5CCEe1B9297d242A2f0836379470D2e857C'
 
   // fx_Treasury: '0x908f0F329d4cab59Ae962b9ba119d7524fDF002B',
   // fx_ETHGateway: '0x38E0C09F0827326d1Cd603Da150346b2597b2792',
@@ -46,8 +46,8 @@ const contracts = {
 
 const tokens = {
   ...tokensInfo.tokens,
-  fETH: '0x6b8dBaB9605FD4a1007e6F64176DB64482B8dB47',
-  xETH: '0x828849cD2f08B3c1ACaD6587B3adb470D870DE16',
+  fETH: '0xd6e6561Ed9D4a041a2e0c771A06aCCA82765635C',
+  xETH: '0x1CCdE657685926e342D12dF5F33d555985FBd0C3',
 }
 
 const TOKENS_INFO = {
