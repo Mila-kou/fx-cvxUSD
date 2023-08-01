f499e769403bf3067f5eda82391a341cfc4813e3
Andrew <Andrew@aladdin>
2023-08-01 08:36:00 +0800

    Update title

--- modules/ido/IdoPage.jsx
+++ modules/ido/IdoPage.jsx
@@ -251,7 +251,7 @@ export default function IdoPage() {
         <div className={styles.container}>
           {[0].includes(newStatus) && (
             <div className={styles.card}>
-              <p className={styles.title}>FX AladdinDAO Offering</p>
+              <p className={styles.title}>FX AladdinDAO Offering Round 2</p>
 
               <p className={styles.num}>{PageData.capAmount} FX</p>
               <p className={styles.title}>Offering Amount</p>
@@ -351,7 +351,7 @@ export default function IdoPage() {
       {!!isWhiteListSoldEndSale && (
         <div className={styles.container}>
           <div className={styles.card}>
-            <p className={styles.title}>FX AladdinDAO Offering</p>
+            <p className={styles.title}>FX AladdinDAO Offering Round 2</p>
 
             <p className={styles.num}>Sold Out</p>
             <p className={styles.title}>
