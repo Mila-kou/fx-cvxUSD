af33c818c3d2712b04e38a52e67023c309c5065e
Andrew <Andrew@aladdin>
2023-09-06 09:09:24 +0800

    Update ETH stETH price

--- modules/home/HomePage.jsx
+++ modules/home/HomePage.jsx
@@ -18,18 +18,27 @@ import abi from '@/config/abi'
 export default function HomePage() {
   const { showSystemStatistics } = useGlobal()
   const [ethPrice, setEthPrice] = useState(0)
+  const [stEthPrice, setStEthPrice] = useState(0)
+
   const { _currentAccount } = useWeb3()
   const [priceLoading, setPriceLoading] = useState(0)
-  const theAddr = '0x91dF41002b9b2f4DB6E5376Af0F338a586c89d8A'
-  const { contract: MockTwapOracleContract } = useContract(
-    theAddr,
+  const ethMockTwapOracleAddr = '0x84496fc45b3b3fda16cc8786bfa07674ac556e84'
+  const stEthMockTwapOracleAddr = '0x0f9d2ba589a9257f8432e88ff6d12a2ff684dc3c'
+
+  const { contract: ethMockTwapOracleContract } = useContract(
+    ethMockTwapOracleAddr,
     abi.MockTwapOracleAbi
   )
+  const { contract: stETHMockTwapOracleContract } = useContract(
+    stEthMockTwapOracleAddr,
+    abi.MockTwapOracleAbi
+  )
+
   const handleSetPrice = async () => {
     try {
       setPriceLoading(true)
       const _minOut = cBN(ethPrice).toString(10)
-      let apiCall = await MockTwapOracleContract.methods.setPrice(_minOut)
+      const apiCall = await ethMockTwapOracleContract.methods.setPrice(_minOut)
 
       const estimatedGas = await apiCall.estimateGas({
         from: _currentAccount,
@@ -48,22 +57,60 @@ export default function HomePage() {
       noPayableErrorAction(`error_mint`, error)
     }
   }
+  const handleSetStETHPrice = async () => {
+    try {
+      setPriceLoading(true)
+      const _minOut = cBN(ethPrice).toString(10)
+      const apiCall = await stETHMockTwapOracleContract.methods.setPrice(
+        _minOut
+      )
+
+      const estimatedGas = await apiCall.estimateGas({
+        from: _currentAccount,
+      })
+      const gas = parseInt(estimatedGas * 1.2, 10) || 0
+      await NoPayableAction(
+        () => apiCall.send({ from: _currentAccount, gas }),
+        {
+          key: 'Price',
+          action: 'Price',
+        }
+      )
+      setPriceLoading(false)
+    } catch (error) {
+      setPriceLoading(false)
+      noPayableErrorAction(`error_mint`, error)
+    }
+  }
+
   const handleChange_CurrentETHPrice = (v) => {
     if (checkNotZoroNum(v)) {
       setEthPrice(v.toString(10))
     }
   }
+  const handleChange_CurrentStETHPrice = (v) => {
+    if (checkNotZoroNum(v)) {
+      setStEthPrice(v.toString(10))
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <Swap />
-        {/* New ETH Price: <SimpleInput
-          onChange={handleChange_CurrentETHPrice}
-        />{' '}
-        <br />
+        New ETH Price: <SimpleInput onChange={handleChange_CurrentETHPrice} />
         <Button width="100%" loading={priceLoading} onClick={handleSetPrice}>
-          OK
-        </Button> */}
+          ETH Price
+        </Button>
+        New stETH Price:{' '}
+        <SimpleInput onChange={handleChange_CurrentStETHPrice} /> <br />
+        <Button
+          width="100%"
+          loading={priceLoading}
+          onClick={handleSetStETHPrice}
+        >
+          stETH Price
+        </Button>
       </div>
       {showSystemStatistics ? (
         <div className={styles.item}>
