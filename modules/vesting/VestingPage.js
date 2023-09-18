6fcafe4ffb5be6598dc95960267a100f9e8a6f59
fefe <chomkw@gmail.com>
2023-09-18 19:08:59 +0800

    feat: Vesting style

--- modules/vesting/VestingPage.js
+++ modules/vesting/VestingPage.js
@@ -6,6 +6,7 @@ import useVesting from './hook/useVesting'
 import { useAladdinClevVest } from '@/hooks/useContracts'
 import useWeb3 from '@/hooks/useWeb3'
 import useGlobal from '@/hooks/useGlobal'
+import styles from './styles.module.scss'
 
 export default function VestingPage() {
   const { theme } = useGlobal()
@@ -49,86 +50,107 @@ export default function VestingPage() {
   function InfoItem({ title, value, unit }) {
     return (
       <div className="my-2">
-        {title}:{' '}
-        <span className={cn(theme === 'blue' ? 'text-red' : 'text-white')}>
-          {value}
-        </span>{' '}
+        {title}: <span className="text-[var(--blue-color)]">{value}</span>{' '}
         {unit}
       </div>
     )
   }
 
+  const itemData = [
+    {
+      title: 'Total No FXN Tokens',
+      value: totalClaimAble,
+    },
+    {
+      title: 'Claimable',
+      value: canClaimText,
+    },
+    {
+      title: 'Not Yet Vested',
+      value: notYetVestedText,
+    },
+    {
+      title: 'Claimed Amount',
+      value: totalClaimAble,
+    },
+  ]
+
   return (
-    <div className="flex flex-col text-center text-lg md:text-xl">
-      <div className="mt-32 mb-16 text-[36px]">Claim FXN Tokens</div>
-      <div className="mb-12">
-        <div
-          className={cn(
-            'mb-4 font-bold',
-            theme === 'blue' ? 'text-red' : 'text-white'
-          )}
-        >
-          Your Vesting
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <h2>Claim FXN Tokens</h2>
+          <div className={styles.items}>
+            {itemData.map((item) => (
+              <div className={styles.item} key={item.title}>
+                <p>{item.title}</p>
+                <h2>{item.value}</h2>
+              </div>
+            ))}
+          </div>
+
+          <div className="flex justify-between pt-10 items-center">
+            <div className="">
+              {!!startTime && (
+                <InfoItem title="Start Time" value={`${startTimeText}`} />
+              )}
+              {!!latestTime && (
+                <InfoItem title="End Time" value={`${latestTimeText}`} />
+              )}
+            </div>
+            {!!(canClaim * 1) && (
+              <div className="flex justify-center">
+                <Button
+                  className="w-40"
+                  onClick={handleClaim}
+                  loading={claiming}
+                  theme="lightBlue"
+                >
+                  Claim
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
-        <InfoItem
-          title="Total No FXN Tokens"
-          value={`${totalClaimAble}`}
-          unit="FXN"
-        />
-        <InfoItem title="Claimable" value={`${canClaimText}`} unit="FXN" />
-        <InfoItem
-          title="Not Yet Vested"
-          value={`${notYetVestedText}`}
-          unit="FXN"
-        />
-        <InfoItem title="Claimed" value={`${claimedAmount}`} unit="FXN" />
-        {!!startTime && (
-          <InfoItem title="Start Time" value={`${startTimeText}`} />
-        )}
-        {!!latestTime && (
-          <InfoItem title="End Time" value={`${latestTimeText}`} />
-        )}
       </div>
 
-      {!!(newList && newList.length) && (
-        <table className={cn('border-collapse border mx-auto', 'text-white')}>
+      <div className="bg-[var(--background-color)] mt-8 p-[56px] rounded-[10px]">
+        <table
+          className={cn(
+            'border-collapse border mx-auto',
+            theme === 'blue' ? 'text-white' : 'text-[#231f20]'
+          )}
+        >
           <thead>
-            <th className={cn('border p-4')}>Start Date</th>
-            <th className={cn('border p-4')}>End Date</th>
-            <th className={cn('border p-4')}>Total Tokens</th>
-            <th className={cn('border p-4')}>% Vested</th>
+            <th className={cn('border py-4 px-12')}>Start Date</th>
+            <th className={cn('border py-4 px-12')}>End Date</th>
+            <th className={cn('border py-4 px-12')}>Total Tokens</th>
+            <th className={cn('border py-4 px-12')}>% Vested</th>
           </thead>
           <tbody>
-            {newList.map((item) => {
-              return (
-                <tr>
-                  <td className={cn('border p-4')}>{item.startTime}</td>
-                  <td className={cn('border p-4')}>{item.endTime}</td>
-                  <td className={cn('border p-4')}>{item.vestingAmount} FXN</td>
-                  <td className={cn('border p-4')}>
-                    {item.vestingAmountPercent}
-                  </td>
-                </tr>
-              )
-            })}
+            {newList && newList.length ? (
+              newList.map((item) => {
+                return (
+                  <tr>
+                    <td className={cn('border py-4 px-12')}>
+                      {item.startTime}
+                    </td>
+                    <td className={cn('border py-4 px-12')}>{item.endTime}</td>
+                    <td className={cn('border py-4 px-12')}>
+                      {item.vestingAmount} FXN
+                    </td>
+                    <td className={cn('border py-4 px-12')}>
+                      {item.vestingAmountPercent}
+                    </td>
+                  </tr>
+                )
+              })
+            ) : (
+              <div className="p-[10px]">No record</div>
+            )}
           </tbody>
         </table>
-      )}
-      <br />
-      <br />
-      {!!(canClaim * 1) && (
-        <div className="flex justify-center">
-          <Button
-            className="w-64"
-            onClick={handleClaim}
-            loading={claiming}
-            theme="lightBlue"
-          >
-            Claim
-          </Button>
-        </div>
-      )}
-      <br />
+      </div>
     </div>
   )
 }
