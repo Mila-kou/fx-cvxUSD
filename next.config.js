77ee3c476820bfff2c1b9ffbdecb3604cb769522
Andrew <Andrew@aladdin>
2023-09-25 22:10:41 +0800

    Update fleek config

--- next.config.js
+++ next.config.js
@@ -16,23 +16,23 @@ module.exports = {
     NETWORK_ENV: process.env.NETWORK_ENV,
     VERSION: version,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/INCH_HOST/:path*',
-        destination: 'https://api.1inch.dev/swap/v5.2/1/:path*',
-      },
-    ]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: false,
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/INCH_HOST/:path*',
+  //       destination: 'https://api.1inch.dev/swap/v5.2/1/:path*',
+  //     },
+  //   ]
+  // },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/home',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
   exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       '/': { page: '/vaults' },
