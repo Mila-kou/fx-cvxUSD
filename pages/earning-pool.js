9d3ae273bf4448a088bdfaea4729c5bc15ceff55
fefe <chomkw@gmail.com>
2023-12-12 16:28:15 +0800

    fix

--- pages/earning-pool.js
+++ pages/earning-pool.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-// import EarningPoolPage from '@/modules/earningPool/EarningPoolPage'
+import EarningPoolPage from '@/modules/earningPool/EarningPoolPage'
 
 export default function EarningPool() {
   return (
@@ -8,7 +8,7 @@ export default function EarningPool() {
       <Head>
         <title>f(x) Protocol</title>
       </Head>
-      {/* <EarningPoolPage /> */}
+      <EarningPoolPage />
     </React.Fragment>
   )
 }
